4698b78821fe68df3e1f435a8cab7bc03e4681e5
kyla <kyla123ma@gmail.com>
2026-01-31 00:59:51 -0800

    Added and modified Perl source code

---
+++ public_html/cgi-bin/perl-hello-html-world.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+print "Cache-Control: no-cache\n";
+print "Content-Type: text/html\n\n";
+
+print "<!DOCTYPE html>";
+print "<html>";
+print "<head>";
+print "<title>Kyla's Hello CGI World</title>";
+print "</head>";
+print "<body>";
+
+print "<h1 align=center>Kyla's Hello HTML World</h1><hr/>";
+print "<p>Hello World -from Kyla</p>";
+print "<p>This page was generated with the Perl programming langauge</p>";
+
+$date = localtime();
+print "<p>This program was generated at: $date</p>";
+
+# IP Address is an environment variable when using CGI
+$address = $ENV{REMOTE_ADDR};
+print "<p>Your current IP Address is: $address</p>";
+
+print "</body>";
+print "</html>";
