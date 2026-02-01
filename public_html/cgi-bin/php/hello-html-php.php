d5109423ab5687cf08ca9ef25c13d7b94670e153
kyla <kyla123ma@gmail.com>
2026-01-31 16:08:14 -0800

    Made title apper in hello-html-php

--- public_html/cgi-bin/php/hello-html-php.php
+++ public_html/cgi-bin/php/hello-html-php.php
@@ -5,7 +5,8 @@ header("Content-Type: text/html");
 <html>
 <head><title>PHP Hello HTML</title></head>
 <body>
-    <h1>Hello! From Kyla</h1>
+    <h1 style="text-align: center">Hello HTML World</h1><hr/>
+    <p>Hello! From Kyla</p>
     <p>This page was generated with the PHP programming language</p>
     <p>This program was generated at: <?php echo date('Y-m-d H:i:s'); ?></p>
     <p>Your current IP Address is: <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
