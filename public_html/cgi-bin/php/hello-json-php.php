de3c185578492164369648c0dd4b6811172683c1
kyla <kyla123ma@gmail.com>
2026-01-31 16:25:29 -0800

    Fixed error in hello-json-php.php

--- public_html/cgi-bin/php/hello-json-php.php
+++ public_html/cgi-bin/php/hello-json-php.php
@@ -3,7 +3,7 @@ header("Content-Type: application/json");
 $data = [
     "title" => "PHP Hello HTML",
     "heading" => "Hello HTML World",
-    "message" => "This page was generated with the PHP programming language"
+    "message" => "This page was generated with the PHP programming language",
     "time" => date('Y-m-d H:i:s'),
     "IP" => $_SERVER['REMOTE_ADDR']
 ];
