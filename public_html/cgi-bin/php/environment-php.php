9ffd7e24af9010cd8571f4518eab53574c48c005
kyla <kyla123ma@gmail.com>
2026-01-31 16:34:14 -0800

    Removed heading from environment-php

--- public_html/cgi-bin/php/environment-php.php
+++ public_html/cgi-bin/php/environment-php.php
@@ -6,10 +6,8 @@ header("Content-Type: text/html");
 </head><body><h1 style="text-align: center">Environment Variables</h1>
 <hr>
 <?php
-echo "<h1>Environment Variables</h1><ul>";
 foreach ($_SERVER as $key => $value) {
-    echo "<li><strong>$key:</strong> $value</li>";
+    echo "<p><strong>$key:</strong> $value</p>";
 }
-echo "</ul>";
 ?>
 </body></html>
