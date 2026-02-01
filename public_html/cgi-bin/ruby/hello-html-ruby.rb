bd3ec67913fd8eb22ff89939973e7e45658fb7b8
kyla <kyla123ma@gmail.com>
2026-02-01 12:31:52 -0800

    Added hello-html-ruby

---
+++ public_html/cgi-bin/ruby/hello-html-ruby.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+require 'date'
+
+puts "Content-Type: type/html\n"
+puts <<~HTML
+<!DOCTYPE html>
+<html>
+<head><title>Ruby Hello HTML</title></head>
+<body>
+    <h1 style="text-align: center">Hello, Ruby!</h1><hr/>
+    <p>Hello! From Kyla</p>
+    <p>This page was generated with the Ruby programming language</p>
+    <p>This program was generated at: #{Time.now.strftime('%Y-%m-%d %H:%M:%S')}</p>
+    <p>Your current IP Address is: #{ENV['REMOTE_ADDR']}</p>
+</body>
+</html>
+HTML
