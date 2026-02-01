c5eef2073ce6b9766fae92d961ecc9aab5b578b7
kyla <kyla123ma@gmail.com>
2026-02-01 03:11:15 -0800

    Added hello-html-python

---
+++ public_html/cgi-bin/python/hello-html-python.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import os
+from datetime import datetime
+
+print("Content-Type: type/html\n")
+print(f"""
+<!DOCTYPE html>
+<html>
+<head><title>Python Hello HTML</title></head>
+<body>
+    <h1 style="text-align: center">Hello HTML World</h1><hr/>
+    <p>Hello! From Kyla</p>
+    <p>This page was generated with the Python programming language</p>
+    <p>This program was generated at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}</p>
+    <p>Your current IP Address is: {os.environ.get('REMOTE_ADDR')}</p>
+</body>
+</html>
+""")
