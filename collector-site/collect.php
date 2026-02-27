ef56c30542f965ce8dbf46b6f3b2b144a4300cc5
kyla <kyla123ma@gmail.com>
2026-02-26 16:27:31 -0800

    Handle origin dynamically in collect.php

--- collector-site/collect.php
+++ collector-site/collect.php
@@ -1,8 +1,13 @@
 <?php
 $allowed_origin = 'https://test.kylacse135.site'; 
-header("Access-Control-Allow-Origin: $allowed_origin");
+$request_origin = $_SERVER['HTTP_ORIGIN'] ?? '';
+
+if ($request_origin === $allowed_origin) {
+    header("Access-Control-Allow-Origin: $request_origin");
+}
+
 header("Access-Control-Allow-Methods: POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Headers: Content-Type, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
